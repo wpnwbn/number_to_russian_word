d4784d6d30b2997e054e5c7aef085fc520fb94c3
Runov Konstantin <wpnwbn@gmail.com>
2021-07-19 14:45:48 +0300

    ADD: multi-column print

--- number_to_russian_word.py
+++ number_to_russian_word.py
@@ -3,6 +3,9 @@ import re
 with open("russian.txt", "r") as f:
     words = list(map(lambda x: x[:-1], f.readlines()))
 
+def ceil(number):
+    return (number // 1) + bool(number % 1)
+
 # Digit-Letter Code
 DLC = {
         '0' : '[нм]',
@@ -26,8 +29,37 @@ while True:
     number = list(map(lambda x: DLC[x], input()))
     regex = sep + f"{sep}".join(number) + sep
 
+    # Matched words
+    match = []
+
     for word in words:
         x = re.fullmatch(regex, word)
-        if x: print(x.group())
+        if x: match.append(x.group())
 
+    # Number of columns that will be displayed
+    columns = 12
+
+    # Rows are chosen so that:
+    # (rows * columns) > len(match)
+    rows = int(ceil(len(match) / columns))
+
+    tmp_table = []
+    for i in range(columns):
+        tmp_table.append(match[i * rows : (i + 1) * rows])
+
+    # Adding empty spaces for tmp_table to be matrix, to not get IndexError when dealing with the last row
+    tmp_table[columns - 1] += [''] * (len(tmp_table[0]) - len(tmp_table[columns - 1]))
+
+    # Tranposing matrix:
+    # [ 1 2 ]     [ 1 3 5 ]
+    # [ 3 4 ] --> [ 2 4 6 ]
+    # [ 5 6 ]
+    # so that the words will be shown sorted by columns, not rows
+    table = [[tmp_table[i][j] for i in range(columns)] for j in range(rows)]
+
+    # Printing final table
+    for i in range(rows):
+        for j in range(columns):
+            print("{:12}".format(table[i][j]), end="")
+        print()
     print()
